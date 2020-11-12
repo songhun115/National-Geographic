1ce8815bc923ec0992884c4e985e497868bd59d1
songhun115 <hoho77113@daum.net>
2020-11-12 14:36:39 +0900

    s

--- main.js
+++ main.js
@@ -35,3 +35,46 @@ $(function(){
     	return window.pageYOffset || document.documentElement.scrollTop;
     }
 });
+
+/* slider  */
+const btn = $('.sliders_box > .side-btns > .lift_btn > .fas');
+btn.click(init);
+function init(){
+  console.log('hi');
+}
+
+const SbsSlider = $('.sliders_box > .side-btns > div');
+function SbsSlider__onClickPostBtn() {
+  console.log('hi');
+    var $this = $(this);
+    var index = $this.index();
+    
+    // 내가 왼쪽으로 이동해야 되나?
+    var isLeft = index == 0;
+    
+    var $slider = $this.closest('.sliders_box');
+    
+    var $current = $slider.find(' > .sliders > slider_item.active');
+    var $post;
+    
+    if ( isLeft ) {
+        $post = $current.prev();
+    }
+    else {
+        $post = $current.next();
+    }
+    
+    if ( $post.length == 0 ) {
+        if ( isLeft ) {
+            $post = $slider.find(' > .sliders > slider_item:last-child');
+        }
+        else {
+            $post = $slider.find(' > .sliders > slider_item:first-child');
+        }
+    }
+    
+    $current.removeClass('active');
+    $post.addClass('active');
+}
+
+SbsSlider.click(SbsSlider__onClickPostBtn);
